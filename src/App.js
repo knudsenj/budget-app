5c598b8cc1d24bf1e6b1f58014dbc001a6f98a2b
Joel Knudsen <knudsen.joel@gmail.com>
2022-01-15 12:22:17 -0800

    Add base layout.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Button, Container, Stack } from "react-bootstrap";
+
 function App() {
-  return 'it works!';
+  return <Container className="my-4">
+    <Stack direction="horizontal" gap="2" className="mb-4">
+      <h1 className="me-auto">Budgets</h1>
+      <Button variant="primary">Add Budget</Button>
+      <Button variant="outline-primary">Add Expense</Button>
+    </Stack>
+    <div className="budget-grid">
+      
+    </div>
+  </Container>;
 }
 
 export default App;
